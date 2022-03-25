15f30084186b7e23b6bdccefbc6d7ed86f1511d9
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-03-25 19:36:34 +0400

    create fake data

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Shop.css'
 
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+
     return (
         <div>
             <h1 className='title-text'><i>Welcome To Laptop Store</i></h1>
             <h2 className='text-content'><i>Choose 4 Laptops</i></h2>
+
         </div>
     );
 };

2b1e6c85627bbff2fe34b3da77e00a9257bf04d4
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-03-25 18:59:16 +0400

    shop component added

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Shop.css'
+
+const Shop = () => {
+    return (
+        <div>
+            <h1 className='title-text'><i>Welcome To Laptop Store</i></h1>
+            <h2 className='text-content'><i>Choose 4 Laptops</i></h2>
+        </div>
+    );
+};
+
+export default Shop;
